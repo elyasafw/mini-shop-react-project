9c8d184e11b6ade9cdf06a10c01a78d9d7d3fd5d
elyasaf waisman <elyasafw1997@gmail.com>
2026-09-06 15:33:23 +0300

    added routes and init app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./Layout";
+import FavoritesPage from "./pages/FavoritesPage";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
-
-  return (
-    <>
-   
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<HomePage />} />
+                        <Route
+                            path="products/:id"
+                            element={<ProductPage />}
+                        ></Route>
+                        <Route path="favorites" element={<FavoritesPage />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
-export default App
+export default App;
